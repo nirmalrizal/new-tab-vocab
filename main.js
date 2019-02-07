9c76dc25b7c049ad50a2ebc80a3a8a8a7d8d4d3f
Nirmal Rijal <nirmalrijal41@gmail.com>
2019-02-07 08:32:24 +0545

    Add dark mode

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const GRE_VOCABULARY_ENDPOINT = `https://gre.economist.com/gre-vocabulary.json`;
 
+const darkModeSwitch = document.getElementById('toggle-dark-mode');
+
 async function main() {
   let words;
   if (localStorage.GRE_VOCABULARY) {
     words = JSON.parse(localStorage.GRE_VOCABULARY);
   } else {
     const res = await fetch(GRE_VOCABULARY_ENDPOINT, {
-      headers: { accept: "application/json" }
+      headers: { accept: 'application/json' }
     });
     words = await res.json();
 
@@ -22,34 +24,34 @@ async function main() {
   word = word.trim();
 
   // Remove whitespace and empasize current word
-  passage = passage.trim().replace(new RegExp(`(${word}[a-z]*)`, "gi"), "$1");
+  passage = passage.trim().replace(new RegExp(`(${word}[a-z]*)`, 'gi'), '$1');
 
   //old node with loading screen
-  var oldVocab = document.getElementsByClassName("vocab")[0];
+  var oldVocab = document.getElementsByClassName('vocab')[0];
 
   //new parent node vocab
-  var vocab = document.createElement("div");
-  vocab.className = "vocab";
+  var vocab = document.createElement('div');
+  vocab.className = 'vocab';
 
   //word node
-  var vocabWord = document.createElement("h1");
-  vocabWord.className = "vocab__word";
+  var vocabWord = document.createElement('h1');
+  vocabWord.className = 'vocab__word';
   var textWord = document.createTextNode(word);
   vocabWord.appendChild(textWord);
 
   //hr node
-  var hr = document.createElement("hr");
-  hr.className = "vocab__hr";
+  var hr = document.createElement('hr');
+  hr.className = 'vocab__hr';
 
   //passage node
-  var vocabPassage = document.createElement("blockquote");
-  vocabPassage.className = "vocab__passage";
+  var vocabPassage = document.createElement('blockquote');
+  vocabPassage.className = 'vocab__passage';
   var textPassage = document.createTextNode(`${passage}`);
   vocabPassage.appendChild(textPassage);
 
   //definition node
-  var vocabDef = document.createElement("p");
-  vocabDef.className = "vocab__definition";
+  var vocabDef = document.createElement('p');
+  vocabDef.className = 'vocab__definition';
   var textDef = document.createTextNode(`${pronunciation} [${partOfSpeech}] - ${definition}
   `);
   vocabDef.appendChild(textDef);
@@ -64,4 +66,38 @@ async function main() {
   document.body.replaceChild(vocab, oldVocab);
 }
 
+function toggleDarkMode(e) {
+  e.preventDefault();
+  if (document.querySelector('body').className.includes('dark')) {
+    setVocabTheme('light');
+  } else {
+    setVocabTheme('dark');
+  }
+}
+
+function setVocabTheme(theme) {
+  const bodySel = document.querySelector('body');
+  const switchInputSel = darkModeSwitch.getElementsByClassName(
+    'switch-input'
+  )[0];
+  bodySel.className = theme;
+  localStorage.GRE_VOCABULARY_THEME = theme;
+  switchInputSel.checked = theme === 'dark';
+}
+
+function checkUserTheme() {
+  if (
+    localStorage.GRE_VOCABULARY_THEME &&
+    localStorage.GRE_VOCABULARY_THEME === 'dark'
+  ) {
+    setVocabTheme('dark');
+  } else {
+    setVocabTheme('light');
+  }
+  darkModeSwitch.style.display = 'block';
+}
+
+darkModeSwitch.addEventListener('click', toggleDarkMode);
+
+checkUserTheme();
 main();
